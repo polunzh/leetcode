716abb4627c19a15bf04dd5e5496057670e40a9a
polunzh <polunzh@gmail.com>
2021-11-18 00:31:43 +0800

    [go] format code

--- go/find-all-numbers-disappeared-in-an-array/main_test.go
+++ go/find-all-numbers-disappeared-in-an-array/main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestIsSubsequence(t *testing.T){
+func TestIsSubsequence(t *testing.T) {
 	assert.Equal(t, []int{5, 6}, findDisappearedNumbers([]int{4, 3, 2, 7, 8, 2, 3, 1}))
-	assert.Equal(t, []int{2}, findDisappearedNumbers([]int{1,1}))
+	assert.Equal(t, []int{2}, findDisappearedNumbers([]int{1, 1}))
 }
